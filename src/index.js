4aedada88833233a5ca7dc9e4bd9a4e906d21471
Sergey <xxsmotur@gmail.com>
2020-01-07 23:30:12 +0500

    Parsing of data

--- src/index.js
+++ src/index.js
@@ -9,14 +9,50 @@ var parse = function(data) {
     }
     if(data.startsWith('at')) {
         var result = data.split('at');
-        var date = result.slice(-1);
-
+        var rawDate = result.slice(-1)[0].trim();
+        var date = makeDate(rawDate);
+        console.log(date);
     }
 }
 
 // parsing of the datetime in format 12:50 
-var parsedateTime = function(dateTime) {
+var parseDateTime = function(dateTime) {
+    if(typeof dateTime != 'string') {
+        return
+    }
 
+    var result = dateTime.split(':');
+    if(result.length < 2) {
+        return
+    }
+    var hour = result[0];
+    var minutes = result[1];
+    if(result.length > 2) {
+        var seconds = result[2];
+    }
+
+    return function() {
+        getHour: {
+            return hour;
+        }
+
+        getMinutes: {
+            return minutes;
+        }
+
+        getSeconds: {
+            return seconds;
+        }
+    }
+}
+
+function Date() {
+   
+}
+
+function makeDate(data) {
+    var result = parseDateTime(data);
+    return new Date(result);
 }
 
 parse('at 12:50');
